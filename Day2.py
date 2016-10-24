0b5e276810a68db81f795bfec0817d7672acfa72
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-10-24 07:46:59 +0200

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,13 @@
+meal_cost = float(raw_input())
+tip_percent = int(raw_input())
+tax_percent = int(raw_input())
+
+
+tip = (meal_cost *float(tip_percent)/100)
+tax = (meal_cost *float(tax_percent)/100)
+
+
+
+total_cost =  meal_cost + tax + tip
+
+print "The total meal cost is" , int(round(total_cost)) ,"dollars."
