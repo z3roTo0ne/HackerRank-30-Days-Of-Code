c1bbd2a2ad8a40e81825a9c53969ce019896c0bb
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-10-26 19:28:41 +0200

    Create Day5.c

---
+++ Day5.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,i; 
+    scanf("%d",&n);
+    
+    for(i = 1; i <= 10; i++){
+        
+        printf("%d x %d = %d\n" , n, i, n*i);
+        
+     }
+        return 0;
+
+}
