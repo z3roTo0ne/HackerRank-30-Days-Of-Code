4159c3c47ef6cc4ae70a903e3ad4c4169ca5e583
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-10-29 13:54:11 +0200

    Create Day6.cpp

---
+++ Day6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+    int t, n =0;
+    
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+     
+        string str;
+
+        cin >> str;  
+        
+
+        for(int i = 0; i < str.length(); i = i+2){
+            
+            cout << str[i];            
+        }
+        
+        
+        cout << " ";
+        
+        for(int i = 1; i < str.length(); i = i+2){
+            
+            cout << str[i]; 
+        }
+        
+        cout << endl;
+      
+        
+     }
+    
+    return 0;
+}
