40927663a1934163acbbbaa89bf0e68b7d626d3e
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-10-29 13:58:06 +0200

    Day 2: Operators

--- Day2.py
+++ Day2.py
@@ -2,11 +2,8 @@ meal_cost = float(raw_input())
 tip_percent = int(raw_input())
 tax_percent = int(raw_input())
 
-
 tip = (meal_cost *float(tip_percent)/100)
 tax = (meal_cost *float(tax_percent)/100)
-
-
 
 total_cost =  meal_cost + tax + tip
 
