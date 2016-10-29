d6bed93b465b958181fa459c4bfc210b162f2a79
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-10-29 14:03:27 +0200

    Day 7: Arrays

--- Day7.c
+++ Day7.c
@@ -7,16 +7,22 @@
 #include <stdbool.h>
 
 int main(){
+    
     int n; 
     scanf("%d",&n);
+    
     int *arr = malloc(sizeof(int) * n);
+    
     for(int arr_i = 0; arr_i < n; arr_i++){
+        
        scanf("%d",&arr[arr_i]);
+        
     }
     
     for( int i = n-1; i >= 0; i--){
         
         printf("%d " , arr[i]);
     }
+    
     return 0;
 }
