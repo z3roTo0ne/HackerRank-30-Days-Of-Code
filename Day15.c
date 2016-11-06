5cb7095a0801c3a987fe292b8425b480eca75e3f
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-11-06 23:31:52 +0200

    Day 15: Linked List

---
+++ Day15.c
@@ -0,0 +1,21 @@
+Node* insert(Node *head,int data) {
+
+    Node * newItem = malloc(sizeof(Node));
+    Node * tail = head;
+    
+    newItem->data = data;
+    newItem->next = NULL;
+    
+    if ( !head ) { 
+        return newItem; 
+    }
+          
+    while (tail->next != NULL) {
+        tail = tail->next;
+    }
+ 
+    tail->next = newItem;
+          
+    return head;
+
+}
