7cbf47f58ffbf47c5bafc4680bdf993350c53ba5
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-11-06 22:29:15 +0200

    Day 16: Exceptions - String to Integer

---
+++ Day16.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import sys
+
+S = raw_input().strip()
+
+try:
+    print int(S)
+    
+except ValueError as verr:
+    print "Bad String"
+    
+except Exception as ex:
+    print "Bad String"
