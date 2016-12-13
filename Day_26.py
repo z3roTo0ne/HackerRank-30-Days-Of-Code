afd6a2f68e6bfb5696b555ae5e57a11930a8bbaa
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-12-13 02:12:12 +0200

    Day 26: Nested Logic

---
+++ Day_26.py
@@ -0,0 +1,22 @@
+actual = (raw_input()).split()
+expected = (raw_input()).split()
+
+da = int(actual[0])
+ma = int(actual[1])
+ya = int(actual[2])
+
+de = int(expected[0])
+me = int(expected[1])
+ye = int(expected[2])
+
+fine = 0
+
+if ya > ye:
+    fine = 10000
+elif ya == ye:
+    if ma > me:
+        fine = (ma - me) * 500
+    elif ma == me and da > de:
+        fine = (da - de) * 15
+
+print(fine)
